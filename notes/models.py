3a60bd8f7787ebdb36b67185ae2218b819c034ed
SUBHAM SHAW <64988887+SubhamShaww@users.noreply.github.com>
2025-11-13 13:57:40 +0530

    feat: add CRUD APIs using DRF

---
+++ notes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    language = models.CharField(max_length=10)
+    translated_text = models.TextField(blank=True, null=True)
+    traslated_language = models.CharField(max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
